75b8255fbe77a89cdf3210cbb6445abc5bb47144
vanishekhar78 <vanishekhar78@gmail.com>
2026-01-03 21:35:59 +0530

    Add files via upload

---
+++ practical.2html.js
@@ -0,0 +1,28 @@
+let num = 10;
+
+if (num > 0) {
+  console.log("Number is positive");
+} else if (num < 0) {
+  console.log("Number is negative");
+} else {
+  console.log("Number is zero");
+}
+
+console.log("For loop:");
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+console.log("While loop:");
+let a = 1;
+while (a <= 5) {
+  console.log(a);
+  a++;
+}
+
+console.log("Do...while loop:");
+let b = 1;
+do {
+  console.log(b);
+  b++;
+} while (b <= 5);
